ba16986a529ef817a4bc3bbbab13dcb2523f00d0
Nishant Singh <119221409+iamfreeezing@users.noreply.github.com>
2024-02-09 03:41:51 +0530

    integrated all

--- integrating.py
+++ integrating.py
@@ -3,16 +3,16 @@ import video
 import gptTotext
 import stockvid
 
-title = "boys"
+def createVideo(title):
+    response = gptTotext.generate_script(title)[1]
+    # response = "Rainforests are home to over half of the world's plant and animal species, making them incredibly diverse and important ecosystems. Protecting rainforests is crucial for the preservation of our planet's biodiversity."
+    vid_link = stockvid.generateVideo(title)[1]
+    print(vid_link)
+    video.save_video(vid_link)
+    length = text_to_speech.generate_audio(response)
 
-response = gptTotext.generate_script(title)[1]
-# response = "Rainforests are home to over half of the world's plant and animal species, making them incredibly diverse and important ecosystems. Protecting rainforests is crucial for the preservation of our planet's biodiversity."
-vid_link = stockvid.generateVideo(title)[1]
-print(vid_link)
-video.save_video(vid_link)
-length = text_to_speech.generate_audio(response)
+    print(video.generate_subtitles("temp/record.mp3"))
+    video.combine_videos(['temp/raw.mp4'], length)
 
-print(video.generate_subtitles("temp/record.mp3"))
-video.combine_videos(['temp/raw.mp4'], length)
-
-video.generate_video("temp/final.mp4", "temp/record.mp3", "temp/raw.srt")
+    video.generate_video("temp/final.mp4", "temp/record.mp3", "temp/raw.srt")
+    
